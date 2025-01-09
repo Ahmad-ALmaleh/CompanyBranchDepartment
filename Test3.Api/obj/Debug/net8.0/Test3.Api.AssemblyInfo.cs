7f482bf510ab597eff55c519e06f505d7df36c5b
unknown <ahmadalmaleh99@gmail.com>
2025-01-09 10:31:31 +0300

    The first is after finishing a good section

--- Test3.Api/obj/Debug/net8.0/Test3.Api.AssemblyInfo.cs
+++ Test3.Api/obj/Debug/net8.0/Test3.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test3.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1a68a7909f8665985a346c7206461ffaaae0a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test3.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test3.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
